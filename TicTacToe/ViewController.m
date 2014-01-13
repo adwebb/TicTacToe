c6d7e28dd32e1be975bc959f0bb5276f84e6f418
Andrew Webb <adwebb@gmail.com>
2014-01-12 18:38:28 -0600

    basic AI implemented

--- TicTacToe/ViewController.m
+++ TicTacToe/ViewController.m
@@ -23,7 +23,8 @@
     __weak IBOutlet UILabel *timerLabel;
     __weak IBOutlet UILabel *dragLabel;
     CGAffineTransform transform;
-
+    int turnCount;
+    BOOL aiGame;
     
 }
 
@@ -37,12 +38,21 @@
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
+    UIAlertView *timeAlert = [[UIAlertView alloc] initWithTitle: [NSString stringWithFormat: @"Game Mode:"]
+                                                        message:@"How would you like to play TicTacToe?"
+                                                       delegate:self
+                                              cancelButtonTitle:@"Vs Computer"
+                                              otherButtonTitles:@"Two Players", nil];
+    [timeAlert show];
+
+    
     whichPlayerLabel.text = @"X Player";
     dragLabel.text = @"X";
     dragLabel.textColor = [UIColor blueColor];
     [whichPlayerLabel sizeToFit];
-    [self newTimer];
     transform = dragLabel.transform;
+    turnCount = 0;
+    
 }
 
 -(void) newTimer
@@ -68,12 +78,26 @@
             }
             if (counter <= 0) {
                 
-                UIAlertView *timeAlert = [[UIAlertView alloc] initWithTitle: [NSString stringWithFormat: @"Time Up!"]
+               if(aiGame == NO)
+               {
+                   
+               UIAlertView *timeAlert = [[UIAlertView alloc] initWithTitle: [NSString stringWithFormat: @"Time Up!"]
                                                             message:@"Please switch players."
                                                            delegate:self
                                                   cancelButtonTitle:@"Aw, shucks."
                                                   otherButtonTitles:nil];
-                [timeAlert show];
+               [timeAlert show];
+               }
+               else if(aiGame == YES)
+               {
+                UIAlertView *timeAlert = [[UIAlertView alloc] initWithTitle: [NSString stringWithFormat: @"Time Up!"]
+                                                                       message:@"You lost your turn!"
+                                                                      delegate:self
+                                                             cancelButtonTitle:@"Aw, shucks."
+                                                             otherButtonTitles:nil];
+                 [timeAlert show];
+               }
+                
                 [self.timer invalidate];
             
         }
@@ -97,6 +121,7 @@
                 myLabelEight.text = @"";
                 myLabelNine.text = @"";
                 [self newTimer];
+                turnCount = 0;
                 break;
             case 1:
                 exit(0);
@@ -117,8 +142,139 @@
             dragLabel.text = @"X";
             dragLabel.textColor = [UIColor blueColor];
         }
+
+        [whichPlayerLabel sizeToFit];
         [self newTimer];
+    }else if ([alert.title isEqualToString:@"Game Mode:"])
+    {
+        [self newTimer];
+        switch (buttonIndex)
+        {
+            case 0:
+                aiGame = YES;
+                break;
+            case 1:
+                aiGame = NO;
+                break;
+            default:
+                break;
+                
+        }
+        
+    }else if([alert.message isEqualToString:@"You lost your turn!"])
+    {
+        
+        [self newTimer];
+        [self aiTurn];
     }
+}
+
+-(void) aiTurn
+{
+    NSInteger aiSelection = (arc4random()%9);
+    NSString *winner;
+    UILabel *label;
+    
+    
+    switch (aiSelection)
+    {
+        case 0:
+            if([myLabelOne.text isEqualToString:@""])
+            {
+                label = myLabelOne;
+            }
+            break;
+        case 1:
+            if([myLabelTwo.text isEqualToString:@""])
+            {
+                label = myLabelTwo;
+            }
+            break;
+        case 2:
+            if([myLabelThree.text isEqualToString:@""])
+            {
+                label = myLabelThree;
+            }
+            break;
+        case 3:
+            if([myLabelFour.text isEqualToString:@""])
+            {
+                label = myLabelFour;
+            }
+            break;
+        case 4:
+            if([myLabelFive.text isEqualToString:@""])
+            {
+                label = myLabelFive;
+            }
+            break;
+        case 5:
+            if([myLabelSix.text isEqualToString:@""])
+            {
+                label = myLabelSix;
+            }
+            break;
+        case 6:
+            if([myLabelSeven.text isEqualToString:@""])
+            {
+                label = myLabelSeven;
+            }
+            break;
+        case 7:
+            if([myLabelEight.text isEqualToString:@""])
+            {
+                label = myLabelEight;
+            }
+            break;
+        case 8:
+            if([myLabelNine.text isEqualToString:@""])
+            {
+                label = myLabelNine;
+            }
+            break;
+        default:
+            [self aiTurn];
+            break;
+    }
+    
+    if([label.text isEqualToString:@""])
+    {
+        label.text = [NSString stringWithFormat:@"O"];
+        label.textColor = [UIColor redColor];
+        whichPlayerLabel.text = @"X Player";
+        winner = [self whoOne];
+        [self newTimer];
+        turnCount++;
+    }
+    else{
+        [self aiTurn];
+        }
+    
+    
+    if([winner isEqualToString:@"O"])
+    {
+        UIAlertView *alert = [[UIAlertView alloc] initWithTitle: [NSString stringWithFormat: @"The Computer Wins!"] message:@"Thanks for playing! Please play again soon!" delegate:self cancelButtonTitle:@"Play Again!" otherButtonTitles:@"Quit.", nil];
+        [alert show];
+        [self.timer invalidate];
+        self.timer = nil;
+    }else if(turnCount == 9)
+    {
+        turnCount = 0;
+        UIAlertView *alert = [[UIAlertView alloc] initWithTitle: [NSString stringWithFormat: @"Cats Game! No one wins!"] message:@"Thanks for playing! Please play again soon!" delegate:self cancelButtonTitle:@"Play Again!" otherButtonTitles:@"Quit.", nil];
+        [alert show];
+        [self.timer invalidate];
+        self.timer = nil;
+        
+    }
+    if ([winner isEqualToString:@"X"])
+    {
+        UIAlertView *alert = [[UIAlertView alloc] initWithTitle: [NSString stringWithFormat: @"You Win!"] message:@"Thanks for playing! Please play again soon!" delegate:self cancelButtonTitle:@"Play Again!" otherButtonTitles:@"Quit.", nil];
+        [alert show];
+        [self.timer invalidate];
+        self.timer = nil;
+    }
+    
+    
 }
 
 -(NSString*)whoOne
@@ -161,7 +317,7 @@
     NSString *winner;
     if([label.text isEqualToString:@""])
     {
-        if([whichPlayerLabel.text isEqualToString:@"X Player"])
+        if([whichPlayerLabel.text isEqualToString:@"X Player"] && aiGame == NO)
         {
             dragLabel.textColor = [UIColor redColor];
             label.textColor = [UIColor blueColor];
@@ -172,7 +328,7 @@
             dragLabel.text = @"O";
             
         }
-        else if ([whichPlayerLabel.text isEqualToString:@"O Player"])    {
+        else if ([whichPlayerLabel.text isEqualToString:@"O Player"] && aiGame == NO)    {
             dragLabel.textColor = [UIColor blueColor];
             label.textColor = [UIColor redColor];
             label.text = @"O";
@@ -180,20 +336,43 @@
             whichPlayerLabel.text = @"X Player";
             [whichPlayerLabel sizeToFit];
             dragLabel.text = @"X";
-            
+        }
+        else if (aiGame == YES)
+        {
+            label.textColor = [UIColor blueColor];
+            dragLabel.textColor = [UIColor blueColor];
+            label.text = @"X";
+            whichPlayerLabel.text = @"O Player";
+            [whichPlayerLabel sizeToFit];
+            winner = [self whoOne];
         }
     }
     [self newTimer];
+    turnCount++;
     
     
-    if([winner isEqualToString:label.text])
+    if([winner isEqualToString:@"X"])
     {
-    UIAlertView *alert = [[UIAlertView alloc] initWithTitle: [NSString stringWithFormat: @"The Winner is %@ Player!",winner] message:@"Thanks for playing! Please play again soon!" delegate:self cancelButtonTitle:@"Play Again!" otherButtonTitles:@"Quit.", nil];
-    [alert show];
+        UIAlertView *alert = [[UIAlertView alloc] initWithTitle: [NSString stringWithFormat: @"X Player Wins!"] message:@"Thanks for playing! Please play again soon!" delegate:self cancelButtonTitle:@"Play Again!" otherButtonTitles:@"Quit.", nil];
+        [alert show];
+            [self.timer invalidate];
+            self.timer = nil;
+    }else if([winner isEqualToString:@"X"])
+    {
+        UIAlertView *alert = [[UIAlertView alloc] initWithTitle: [NSString stringWithFormat: @"O Player Wins!"] message:@"Thanks for playing! Please play again soon!" delegate:self cancelButtonTitle:@"Play Again!" otherButtonTitles:@"Quit.", nil];
+        [alert show];
         [self.timer invalidate];
         self.timer = nil;
-    }
-    
+    }else if(turnCount == 9)
+    {
+        turnCount = 0;
+        UIAlertView *alert = [[UIAlertView alloc] initWithTitle: [NSString stringWithFormat: @"Cats Game! No one wins!"] message:@"Thanks for playing! Please play again soon!" delegate:self cancelButtonTitle:@"Play Again!" otherButtonTitles:@"Quit.", nil];
+        [alert show];
+        [self.timer invalidate];
+        self.timer = nil;
+
+    }else if(aiGame == YES && winner == nil && label != nil)
+        [self aiTurn];
 }
 
 -(UILabel*)findLabelUsingPoint:(CGPoint)point
